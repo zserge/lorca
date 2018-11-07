9539f4666a403b8844c32580c9e1f7bae8f23595
Sergii Zaitsev <sergii.zaitsev@sixt.com>
2018-11-07 11:34:43 +0100

    add dynamic value type

---
+++ value.go
@@ -0,0 +1,43 @@
+package lorca
+
+import "encoding/json"
+
+type Value interface {
+	Err() error
+	To(interface{}) error
+	Float() float32
+	Int() int
+	String() string
+	Bool() bool
+	Object() map[string]Value
+	Array() []Value
+}
+
+type value struct {
+	err error
+	raw json.RawMessage
+}
+
+func (v value) Err() error             { return v.err }
+func (v value) To(x interface{}) error { return json.Unmarshal(v.raw, x) }
+func (v value) Float() (f float32)     { v.To(&f); return f }
+func (v value) Int() (i int)           { v.To(&i); return i }
+func (v value) String() (s string)     { v.To(&s); return s }
+func (v value) Bool() (b bool)         { v.To(&b); return b }
+func (v value) Array() (values []Value) {
+	array := []json.RawMessage{}
+	v.To(&array)
+	for _, el := range array {
+		values = append(values, value{raw: el})
+	}
+	return values
+}
+func (v value) Object() (object map[string]Value) {
+	object = map[string]Value{}
+	kv := map[string]json.RawMessage{}
+	v.To(&kv)
+	for k, v := range kv {
+		object[k] = value{raw: v}
+	}
+	return object
+}
---
+++ value_test.go
@@ -0,0 +1,60 @@
+package lorca
+
+import (
+	"encoding/json"
+	"errors"
+	"testing"
+)
+
+var testError = errors.New("fail")
+
+func TestValueError(t *testing.T) {
+	v := value{err: testError}
+	if v.Err() != testError {
+		t.Fail()
+	}
+
+	v = value{raw: json.RawMessage(`"hello"`)}
+	if v.Err() != nil {
+		t.Fail()
+	}
+}
+
+func TestValuePrimitive(t *testing.T) {
+	v := value{raw: json.RawMessage(`42`)}
+	if v.Int() != 42 {
+		t.Fail()
+	}
+	v = value{raw: json.RawMessage(`"hello"`)}
+	if v.Int() != 0 {
+		t.Fail()
+	}
+	v = value{err: testError}
+	if v.Int() != 0 {
+		t.Fail()
+	}
+	v = value{raw: json.RawMessage(`"hello"`)}
+	if v.String() != "hello" {
+		t.Fail()
+	}
+	v = value{err: testError}
+	if v.String() != "" {
+		t.Fail()
+	}
+}
+
+func TestValueComplex(t *testing.T) {
+	v := value{raw: json.RawMessage(`["foo", 42.3, {"x": 5}]`)}
+	if len(v.Array()) != 3 {
+		t.Fail()
+	}
+	if v.Array()[0].String() != "foo" {
+		t.Fail()
+	}
+	if v.Array()[1].Float() != 42.3 {
+		t.Fail()
+	}
+	if v.Array()[2].Object()["x"].Int() != 5 {
+		t.Fail()
+	}
+}
