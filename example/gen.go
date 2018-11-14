52e77bd3d323d7b860b3437f8db2a722db8e1c61
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2018-11-14 22:31:09 +0100

    add asset filesystem, embed assets in the example

---
+++ example/gen.go
@@ -0,0 +1,11 @@
+//+build generate
+
+package main
+
+import "github.com/zserge/lorca"
+
+func main() {
+	// You can also run "npm build" or webpack here, or compress assets, or
+	// generate manifests, or do other preparations for your assets.
+	lorca.Embed("assets.go", "www")
+}
--- example/main.go
+++ example/main.go
@@ -1,65 +1,18 @@
+//go:generate go run -tags generate gen.go
+
 package main
 
 import (
+	"fmt"
 	"log"
-	"net/url"
+	"net"
+	"net/http"
 	"os"
 	"os/signal"
 	"sync"
 
 	"github.com/zserge/lorca"
 )
-
-const html = `
-<!doctype html>
-<html>
-	<head>
-		<title>Counter</title>
-		<style>
-		* { margin: 0; padding: 0; box-sizing: border-box; user-select: none; }
-		body { height: 100vh; display: flex; align-items: center; justify-content: center; }
-		.counter-container { display: flex; flex-direction: column; align-items: center; }
-		.btn-row { display: flex; align-items: center; }
-		.btn { cursor: pointer; min-width: 4em; border: 1px solid black; text-align: center; margin: 0.5rem; }
-		</style>
-	</head>
-	<body onload=start()>
-
-		<!-- UI layout -->
-		<div class="counter-container">
-			<div class="counter"></div>
-			<div class="btn-row">
-				<div class="btn btn-incr">+1</div>
-				<div class="btn btn-decr">-1</div>
-			</div>
-		</div>
-
-		<!-- Connect UI actions to Go functions -->
-		<script type="application/javascript">
-			const counter = document.querySelector('.counter');
-			const btnIncr = document.querySelector('.btn-incr');
-			const btnDecr = document.querySelector('.btn-decr');
-
-			// We use async/await because Go functions are asynchronous
-			const render = async () => {
-				counter.innerText = ` + "`Count: ${await counterValue()}`" + `;
-			};
-
-			btnIncr.addEventListener('click', async () => {
-				await counterAdd(1); // Call Go function
-				render();
-			});
-
-			btnDecr.addEventListener('click', async () => {
-				await counterAdd(-1); // Call Go function
-				render();
-			});
-
-			render();
-		</script>
-	</body>
-</html>
-`
 
 // Go types that are bound to the UI must be thread-safe, because each binding
 // is executed in its own goroutine. In this simple case we may use atomic
@@ -98,8 +51,17 @@ func main() {
 	ui.Bind("counterAdd", c.Add)
 	ui.Bind("counterValue", c.Value)
 
-	// Load HTML, a local web server on a random port would also work
-	ui.Load("data:text/html," + url.PathEscape(html))
+	// Load HTML.
+	// You may also use `data:text/html,<base64>` approach to load initial HTML,
+	// e.g: ui.Load("data:text/html," + url.PathEscape(html))
+
+	ln, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ln.Close()
+	go http.Serve(ln, http.FileServer(FS))
+	ui.Load(fmt.Sprintf("http://%s", ln.Addr()))
 
 	// You may use console.log to debug your JS code, it will be printed via
 	// log.Println(). Also exceptions are printed in a similar manner.
