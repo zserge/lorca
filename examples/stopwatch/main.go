70bdab84f58f1a3d0095e005c4c86d7eeb4029c2
Sergii Zaitsev <sergii.zaitsev@sixt.com>
2018-11-19 15:14:24 +0100

    add stopwatch example

---
+++ examples/stopwatch/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+	"sync/atomic"
+	"time"
+
+	"github.com/zserge/lorca"
+)
+
+func main() {
+	ui, _ := lorca.New("", "", 480, 320)
+	defer ui.Close()
+
+	// Data model: number of ticks
+	ticks := uint32(0)
+	// Channel to connect UI events with the background ticking goroutine
+	togglec := make(chan bool)
+	// Bind Go functions to JS
+	ui.Bind("toggle", func() { togglec <- true })
+	ui.Bind("reset", func() {
+		atomic.StoreUint32(&ticks, 0)
+		ui.Eval(`document.querySelector('.timer').innerText = '0'`)
+	})
+
+	// Load HTML after Go functions are bound to JS
+	ui.Load("data:text/html," + url.PathEscape(`
+	<html>
+		<body>
+			<!-- toggle() and reset() are Go functions wrapped into JS -->
+			<div class="timer" onclick="toggle()"></div>
+			<button onclick="reset()">Reset</button>
+		</body>
+	</html>
+	`))
+
+	// Start ticker goroutine
+	go func() {
+		t := time.NewTicker(100 * time.Millisecond)
+		for {
+			select {
+			case <-t.C: // Every 100ms increate number of ticks and update UI
+				ui.Eval(fmt.Sprintf(`document.querySelector('.timer').innerText = 0.1*%d`,
+					atomic.AddUint32(&ticks, 1)))
+			case <-togglec: // If paused - wait for another toggle event to unpause
+				<-togglec
+			}
+		}
+	}()
+	<-ui.Done()
+}
