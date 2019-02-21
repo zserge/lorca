16ca4d4a6c819aedd926a51869ecda211a6e2fe9
Łukasz Miłkowski <lukanus@uaznia.net>
2019-02-21 16:45:41 +0100

    Update parameters for Embed to match definition

--- examples/counter/gen.go
+++ examples/counter/gen.go
@@ -7,5 +7,5 @@ import "github.com/zserge/lorca"
 func main() {
 	// You can also run "npm build" or webpack here, or compress assets, or
 	// generate manifests, or do other preparations for your assets.
-	lorca.Embed("main", "assets.go", "www")
+	lorca.Embed("assets.go", "www")
 }
