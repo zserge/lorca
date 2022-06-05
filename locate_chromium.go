786f1531c9fa3562f66b9446f98df7210c4f0a23
George Carlson <gcarlson@adc4gis.com>
2022-06-05 12:20:35 -0500

    Seperate Chromium and Edge UI's
    Seperate the chromium and EDGE UI's into lorca.NewChromium and
    lorca.NewEdge so that the user can choose to use Edge instead of
    chrome when they have booth installed. Also, do not automatically
    add in all the default arguments. Instead make these public through
    AdditionalEdgeArgs and AdditionalChromiumArgs so that the user is not
    forced to use all the default arguments.

---
+++ locate_chromium.go
@@ -0,0 +1,62 @@
+package lorca
+
+import (
+	"os"
+	"runtime"
+)
+
+// ChromeExecutable returns a string which points to the preferred Chromium
+// executable file.
+var ChromiumExecutable = LocateChromium
+
+// LocateChromium returns a path to the Chromium binary, or an empty string if
+// Chromium installation is not found.
+func LocateChromium() string {
+
+	// If env variable "LORCACHROME" specified and it exists
+	if path, ok := os.LookupEnv("LORCACHROMIUM"); ok {
+		if _, err := os.Stat(path); err == nil {
+			return path
+		}
+	}
+
+	var paths []string
+	switch runtime.GOOS {
+	case "darwin":
+		paths = []string{
+			"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+			"/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary",
+			"/Applications/Chromium.app/Contents/MacOS/Chromium",
+			"/usr/bin/google-chrome-stable",
+			"/usr/bin/google-chrome",
+			"/usr/bin/chromium",
+			"/usr/bin/chromium-browser",
+		}
+	case "windows":
+		paths = []string{
+			os.Getenv("LocalAppData") + "/Google/Chrome/Application/chrome.exe",
+			os.Getenv("ProgramFiles") + "/Google/Chrome/Application/chrome.exe",
+			os.Getenv("ProgramFiles(x86)") + "/Google/Chrome/Application/chrome.exe",
+			os.Getenv("LocalAppData") + "/Chromium/Application/chrome.exe",
+			os.Getenv("ProgramFiles") + "/Chromium/Application/chrome.exe",
+			os.Getenv("ProgramFiles(x86)") + "/Chromium/Application/chrome.exe",
+		}
+	default:
+		paths = []string{
+			"/usr/bin/google-chrome-stable",
+			"/usr/bin/google-chrome",
+			"/usr/bin/chromium",
+			"/usr/bin/chromium-browser",
+			"/snap/bin/chromium",
+		}
+	}
+
+	for _, path := range paths {
+		if _, err := os.Stat(path); os.IsNotExist(err) {
+			continue
+		}
+		return path
+	}
+	return ""
+}
+
